c68d7a85b09a259a795631eb38d917c4c5f7116c
Sargasm <kolbykruger@gmail.com>
2019-05-23 16:17:20 -0400

    updated slideshow call to fade

--- js/scripts.js
+++ js/scripts.js
@@ -112,15 +112,15 @@ $('iframe[src*="youtube"]').wrap('<div class="responsiveIframe"/>');
 //Slideshow
 $('.slideshow').slick({
   autoplay: true,
-  autoplaySpeed: 5000,
-  dots: true,
+  autoplaySpeed: 8000,
+  dots: false,
   infinite: true,
-  fade: false,
+  fade: true,
   speed: 300,
   slidesToShow: 1,
   slidesToScroll: 1,
-  prevArrow:'<button class="prev" title="Previous Slide"><i class="fal fa-chevron-left"></i></button>',
-  nextArrow:'<button class="next" title="Next Slide"><i class="fal fa-chevron-right"></i></button>'
+  prevArrow:'',
+  nextArrow:''
 });
 
 $(window).on('load resize orientationchange', function() {
